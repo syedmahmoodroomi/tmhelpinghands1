55f1f21f8e979e8da3203877050fa152fc796ce9
Saurabh Agrawal <royalshop.new@gmail.com>
2020-08-11 12:22:19 +0530

    Initial Commit

--- app/src/main/java/com/saurabhdev/tmhelpinghands/MadarsaForYouth.java
+++ app/src/main/java/com/saurabhdev/tmhelpinghands/MadarsaForYouth.java
@@ -1,13 +1,68 @@
 package com.saurabhdev.tmhelpinghands;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MadarsaForYouth extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_madarsa_for_youth);
+        //Window window = getWindow();
+        //window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getSupportActionBar().hide();
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        viewPager = (ViewPager) findViewById(R.id.myViewPager);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupViewPager(viewPager);
+        ImageView homeimage = findViewById(R.id.btnhome);
+        homeimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setupViewPager(viewPager);
+
+            }
+        });
+
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getFragmentManager());
+        viewPagerAdapter.addFragment(new AbuTalib(), "Madarsa For Boys");
+        viewPagerAdapter.addFragment(new KhadeejatulKubra(), "Madarsa For Girls");
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setMessage("Are You Sure You Want to Exit?")
+                .setNegativeButton("NO",null)
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).show();
     }
 }
